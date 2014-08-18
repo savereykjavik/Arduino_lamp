59694b8049c85630784fa37f7c6800af949e0b98
thordenberg <cath@Carolines-MacBook-Air-2.local>
2014-08-19 01:39:23 +0200

    Cleaned up

--- arduino_lamp.ino
+++ arduino_lamp.ino
@@ -1,47 +1,47 @@
 /*
 
-Glow in dark, with fotoresistor.
+Control a switch (connected to a lamp) with a light sensor and an arduino.
 
  */
 
-// Pin 13 is used to turn on the light
-// Pin 12 turn off
-// Pin 0 Analog In reads fotoresistor
+// Pin 13 is used to turn on the switch
+// Pin 12 is used to turn off the switch
+// Pin 0 (Analog In) reads light sensor
 
-// Give them names (no magic numbers) kan stå som int, men const int gör de till konstata tal.
+// Give the pins names
 const int switchOn = 13;
 const int switchOff = 12;
-const int lightmeter = 0; 
+const int lightmeter = 0;
+
+// set the light parameters
+// hysteresis model: http://en.wikipedia.org/wiki/Hysteresis
+// threshold is the "center point" of when the light will switch from on/off and off/on
+// tolerance is the span that separates the on and off trigger levels (to avoid flickering of the lamp)
 const int threshold = 400;
 const int tolerance = 100;
 
-// OBS OBS OBS Global states - variabler som sätts innan setup körs. 
-// Borde reflektera vilket grundstate du vill ha om den deklareras med värde.
-// Declate the state of the lamp. 0 = off, 1 = on.
+// Declate the variable "state" that will serve as an indicator for when the switch is on or off 
+// (0 = off, 1 = on).
 int state;
-
 
 // the setup routine runs once when you press reset:
 void setup() { 
   // initialize the digital pin as an output/inputs.
   pinMode(switchOn, OUTPUT);
   pinMode(switchOff, OUTPUT);
+  // call turnOff to start with the switch being off
   turnOff();
 }
 
-// the loop routine runs over and over again forever:
+// this loop routine runs over and over again forever to read the value of the light sensor:
 void loop() {
   int light = analogRead(lightmeter);
-  
-  // Obs if the if/else bodies contain only one statement, no blocks { } are needed.
   if      (state == 1 && light > threshold + tolerance) turnOff();
   else if (state == 0 && light < threshold - tolerance) turnOn();
 }  
 
+// this function turns the switch on, and then changes the state variable to "1"
 void turnOn() {
-  // Precondition: if the lamp is already on, do nothing. The funtion returns/ends.
-  // if (state == 1) return;
-  
   digitalWrite(switchOn, HIGH);
   digitalWrite(switchOff, LOW);
   delay(500);
@@ -49,10 +49,8 @@ void turnOn() {
   state = 1;
 }
 
+//this function turns the switch off, and then changes the state variable to "0"
 void turnOff() {
-  // Precondition: if the lamp is already off, do nothing. The funtion returns/ends.
-  // if (state == 0) return;
-  
   digitalWrite(switchOn, LOW);
   digitalWrite(switchOff, HIGH);
   delay(500);
